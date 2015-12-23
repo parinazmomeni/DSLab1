431c91bbd58b40c76c6df5f721ad8d04bc08117d
Stefan Belk <belk.stefan@gmail.com>
2015-12-23 23:12:29 +0100

    Improved code base

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -3,7 +3,6 @@ package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.DatagramPacket;
